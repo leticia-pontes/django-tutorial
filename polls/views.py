fb6dacede044744add8fde51d25451caafafd894
Leticia Alves de Pontes <50830736+leticia-hub@users.noreply.github.com>
2024-02-21 16:03:19 -0300

    Aula 03 - Gerenciamento de Banco de Dados e Templates

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from .models import Question, Choice
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by("question_text")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
-# Função recebe requisição. Não importa requisição pois ainda não especificou a requisição.
-def index(request: HttpRequest):
-    print(request)
-    print(request.headers)
-    print(request.body)
-    return HttpResponse(
-        f"Hello, World! {request.headers['User-Agent']}",
-        status=404,
-    )
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
